8d30b4a76cba005e335fb2af32f6745e6865a12b
YIPENG WANG <yw3548@nyu.edu>
2021-06-09 17:19:33 +0800

    Add files via upload

--- netValue.py
+++ netValue.py
@@ -1,4 +1,6 @@
 import json
+import csv
+import math
 from tqdm import tqdm
 
 trades = {} #store part of a json dictionary for convinient key hashing
@@ -10,7 +12,7 @@ multiplier  = 10**18 #used as divisor to retrieve real trade values
 def updateNetValues(trade):
     #either create trader netvalue info or update it if existed
     if trade[0] not in netValues:
-        netValues[trade[0]] = (trade[2] * trade[3],0,0,0,0,0)
+        netValues[trade[0]] = [trade[2] * trade[3],0,0,0,0,0]
         if trade[1]== 0:#update account BTC value
             netValues[trade[0]][1] = trade[2] * trade[3]
             netValues[trade[0]][2] = trade[2]
@@ -18,23 +20,79 @@ def updateNetValues(trade):
             netValues[trade[0]][3] = trade[2] * trade[3]
             netValues[trade[0]][4] = trade[2]
     else:
-        if trade[1]== 0:
+        if trade[1]== 0: #BTC 
             #same direction order modifies values but creates no profit change
-            if trade[2] * trade[3]* previousNet>=0:
+            if trade[2] * netValues[trade[0]][2]>=0:
                 netValues[trade[0]][1] += trade[2] * trade[3]
                 netValues[trade[0]][2] += trade[2]
                 netValues[trade[0]][0] += trade[2] * trade[3]
 
             #opposite direction order creates profits change
-            elif trade[2] * trade[3]* previousNet<0:
+            elif trade[2] * netValues[trade[0]][2]<0:
                 averagePrice = netValues[trade[0]][1]/netValues[trade[0]][2]
-                if trade[3]<0: #short order on long position
-                    if trade[3] + averagePrice < 0: #produce profit
-                        if netValues[trade[0]][2]< trade[2]: #position switched
-                            netValues[trade[0]][5] += 
-                        else: #maintain original position status
+                if trade[2]<0: #short order on long position
+              
+                    if netValues[trade[0]][2]< abs(trade[2]): #position switched
+                        netValues[trade[0]][5] += (trade[3] - averagePrice)* netValues[trade[0]][2]
 
-                if trade[3]>0: #long order on short positions 
+                        
+                        netValues[trade[0]][1] = (trade[2]+ netValues[trade[0]][2])* trade[3]
+                        netValues[trade[0]][2] += trade[2]
+
+                    else: #maintain original position status or make a clearance
+                        netValues[trade[0]][5] -= (trade[3] - averagePrice)* trade[2]
+                        netValues[trade[0]][2] += trade[2]
+                        netValues[trade[0]][1] = averagePrice * netValues[trade[0]][2]
+
+                elif trade[2]>0: #long order on short positions 
+
+                    if abs(netValues[trade[0]][2])< trade[2]: #position switched
+                        netValues[trade[0]][5] += (trade[3] - averagePrice)* netValues[trade[0]][2]
+                        netValues[trade[0]][1] = (trade[2]+ netValues[trade[0]][2])* trade[3]
+                        netValues[trade[0]][2] += trade[2]
+                    else: #maintain original position status or make a clearance
+                        netValues[trade[0]][5] -= (trade[3] - averagePrice)* trade[2]
+                        netValues[trade[0]][2] += trade[2]
+                        netValues[trade[0]][1] = averagePrice * netValues[trade[0]][2]
+
+                #update total values
+                netValues[trade[0]][0] = netValues[trade[0]][1] * netValues[trade[0]][2] + netValues[trade[0]][3] * netValues[trade[0]][4]
+                
+
+        elif trade[1]== 1: #ETH
+            #same direction order modifies values but creates no profit change
+            if trade[2] * netValues[trade[0]][4]>=0:
+                netValues[trade[0]][3] += trade[2] * trade[3]
+                netValues[trade[0]][4] += trade[2]
+                netValues[trade[0]][0] += trade[2] * trade[3]
+
+            #opposite direction order creates profits change
+            elif trade[2] * netValues[trade[0]][4]<0:
+                averagePrice = netValues[trade[0]][3]/netValues[trade[0]][4]
+                if trade[2]<0: #short order on long position
+              
+                    if netValues[trade[0]][4]< abs(trade[2]): #position switched
+                        netValues[trade[0]][5] += (trade[3] - averagePrice)* netValues[trade[0]][4]
+                        netValues[trade[0]][3] = (trade[2]+ netValues[trade[0]][4])* trade[3]
+                        netValues[trade[0]][4] += trade[2]
+                    else: #maintain original position status or make a clearance
+                        netValues[trade[0]][5] -= (trade[3] - averagePrice)* trade[2]
+                        netValues[trade[0]][4] += trade[2]
+                        netValues[trade[0]][3] = averagePrice * netValues[trade[0]][4]
+
+                elif trade[2]>0: #long order on short positions 
+
+                    if abs(netValues[trade[0]][4])< trade[2]: #position switched
+                        netValues[trade[0]][5] += (trade[3] - averagePrice)* netValues[trade[0]][4]
+                        netValues[trade[0]][3] = (trade[2]+ netValues[trade[0]][4])* trade[3]
+                        netValues[trade[0]][4] += trade[2]
+                    else: #maintain original position status or make a clearance
+                        netValues[trade[0]][5] -= (trade[3] - averagePrice)* trade[2]
+                        netValues[trade[0]][4] += trade[2]
+                        netValues[trade[0]][3] = averagePrice * netValues[trade[0]][4]
+
+                #update other values
+                netValues[trade[0]][0] = netValues[trade[0]][1] * netValues[trade[0]][2] + netValues[trade[0]][3] * netValues[trade[0]][4]
 
  
 
@@ -52,6 +110,7 @@ def getNetValues(trades):
                 price = tradeInfo["price"]/multiplier
                 time = tradeInfo["timestamp"]
                 trade = (owner, symbol, volume, price, time)
+                print(trade)
                 updateNetValues(trade)
                 
 
@@ -62,3 +121,14 @@ if __name__ == "__main__":
     trades = trades["blocks"]
     getNetValues(trades)
     print(netValues)
+
+    
+    output = open("output.csv","w")
+    writer = csv.writer(output)
+    writer.writerow(["account address","总仓位成本","BTC建仓成本","BTC仓位","ETH建仓成本", "ETH仓位", "利润"])
+    for account in netValues:
+        writer.writerow([account] + netValues[account])
+
+    file.close()
+    output.close()
+  
